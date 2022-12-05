73feb02ac8db05e486fc09dffb568cedd6a78e18
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-05 22:26:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+  let [num1,setNum1]=useState(0)
+  return (
+    <div className="App">
+       <h1>{num1}</h1>
+       <button onClick={()=>{
+        setNum1(num1+1)
+       }}>Click</button>
+    </div>
+  );
+}
+
+export default App;
