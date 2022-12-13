9caf0e6be83dae4f6a97eaa57718d3353a4faec8
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-13 12:44:59 +0530

    here we added css file.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   let [num1,setNum1]=useState(0)
   return (
     <div className="App">
+      <h1>Increasing Value App.</h1>
        <h1>{num1}</h1>
        <button onClick={()=>{
         setNum1(num1+1)
